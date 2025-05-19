bacb56e2fbb7f3e9f4e97974bb2c12fa6fc9ac3a
karinacmartins <nina.kastro@icloud.com>
2025-05-19 20:11:10 -0300

    fix: revert profile data URL to local path

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/karinacmartins/dio-portifolio-js/refs/heads/initial/assets/data/profile.json';
+    const url = '../assets/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
