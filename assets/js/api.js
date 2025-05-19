f7be43b7b3b91699663748cf28c8102878f1cbdf
karinacmartins <nina.kastro@icloud.com>
2025-05-19 20:33:59 -0300

    fix: update profile data URL and restore profile data JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = '/public/profile.json';
+    const url = '/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
